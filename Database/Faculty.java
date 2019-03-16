303f73dfeadb53c0bb571d878145ad214918599c
everest.wild <everest.wild@cs.ucalgary.ca>
2019-03-15 21:00:36 -0600

    Upload New File

---
+++ Database/Faculty.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+public class Faculty {
+  private static ArrayList<Faculty> facultySet = new ArrayList<Faculty>();
+  private String name;
+  private String ID;
+  public ArrayList<Department> departmentSet = new ArrayList<Department>();
+  
+  public Faculty() {
+	  facultySet.add(this);
+  }
+  
+  public boolean setName(String name) {
+	  facultySet.remove(this);
+	  String nameUpper = name.toUpperCase();
+	  for (int i = 0 ; i < facultySet.size();i++) {
+		  if (facultySet.get(i).getName().toUpperCase().equals(nameUpper)) {
+			  facultySet.add(this);
+			  return false;
+		  }
+	  }
+	  this.name = name;
+	  facultySet.add(this);
+	  return true;
+  }
+  
+  public String getName() {
+	  return name;
+  }
+  
+  public String getID() {
+	  return this.ID;
+  }
+  
+  public boolean setID(String ID) {
+	  if (ID.length()!=2) {
+		  return false;
+	  }
+	  try {
+		  Integer.parseInt(ID);
+	  }catch(Exception e) {
+		  return false;
+	  }
+	  facultySet.remove(this);
+	  for(int i = 0; i < facultySet.size();i++) {
+		  if(facultySet.get(i).getID().equals(ID)) {
+			  facultySet.add(this);
+			  return false;
+		  }
+	  }
+	  this.ID = ID;
+	  facultySet.add(this);
+	  return true;
+	  
+  }
+
+  public static ArrayList<Faculty> getFacultySet(){
+	  return facultySet;
+  }
+  
+  public void removeFaculty() {
+	  facultySet.remove(this);
+  }
+}
